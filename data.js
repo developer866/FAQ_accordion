1715c12db02a9e5a1ece4e9ec3ef264967a6df99
developer866 <opeyemijoseph866@gmail.com>
2023-03-15 13:38:57 +0100

    This is an accordion project from frontend mentor

---
+++ data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    question: "How many team members can I invite?",
+    anwser:
+      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+  },
+  {
+    id: 2,
+    question: "What is the maximum file upload size?",
+    anwser:
+      "No more than 2GB. All files in your account must fit your allotted storage space.",
+  },
+  {
+    id: 3,
+    question: "How do I reset my password?",
+    anwser:
+      "Click “Forgot password” from the login page or “Change password” from your profile page.A reset link will be emailed to you.",
+  },
+  {
+    id: 4,
+    question: "Can I cancel my subscription?",
+    anwser:
+      "Yes! Send us a message and we’ll process your request no questions asked.",
+  },
+  {
+    id: 5,
+    question: "Do you provide additional support?",
+    anwser:
+      "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+  },
+];
